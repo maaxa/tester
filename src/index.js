bdbe2f40b1c1394659ae5486dbe68a74126ef101
maaxa <m-a-x-07@yandex.ru>
2019-12-29 22:25:59 +0300

    6 урок
    6 урок

--- src/index.js
+++ src/index.js
@@ -3,14 +3,19 @@ import ReactDOM from "react-dom";
 
 import "./styles.css";
 
-function App() {
+const App = () => {
   return (
     <div className="App">
-      <h1>Hello CodeSandbox</h1>
+      <h1>Привет мир!</h1>
       <h2>Start editing to see some magic happen!</h2>
+      <ul>
+        <li>dfgdfg</li>
+        <li>dfgdfg</li>
+        <li>dfgdfg</li>
+      </ul>
     </div>
   );
-}
+};
 
 const rootElement = document.getElementById("root");
 ReactDOM.render(<App />, rootElement);
